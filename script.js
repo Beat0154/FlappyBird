7d5a5fb34362fae939c497f7ea12b28f29c04a9f
Shawn Beaton <beat0154@algonquinlive.com>
2020-08-26 08:58:10 -0400

    first commit

--- script.js
+++ script.js
@@ -1,46 +1,42 @@
 var block = document.getElementById("block");
-var character = document.getElementById("character");
 var hole = document.getElementById("hole");
-var coin = document.getElementById("coin");
-var counter=0;
-var jumping=0;
+var character = document.getElementById("character");
+var jumping = 0;
+var counter = 0;
+
 hole.addEventListener('animationiteration', () => {
-    let random = -((Math.random()*300)+150);
+    var random = -((Math.random()*300)+150);
     hole.style.top = random + "px";
-    setTimeout(function(){
-        random = -((Math.random()*300)+200);
-        coin.style.top = random + "px";
-    },1000);
     counter++;
 });
-var gravity = setInterval(function(){
+setInterval(function(){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     if(jumping==0){
-        character.style.top = (characterTop+3) + "px";
+        character.style.top = (characterTop+3)+"px";
     }
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
     var cTop = -(500-characterTop);
-    if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>(holeTop+130))))){
-//        alert("game over. score: "+counter);
-        character.style.top = 200 + "px";
-        counter = 0;
+    if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
+        alert("Game over. Score: "+(counter-1));
+        character.style.top = 100 + "px";
+        counter=0;
     }
 },10);
 
 function jump(){
-    let counter = 0;
-    jumping=1;
+    jumping = 1;
+    let jumpCount = 0;
     var jumpInterval = setInterval(function(){
         var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-        if(counter>20){
+        if((characterTop>6)&&(jumpCount<15)){
+            character.style.top = (characterTop-5)+"px";
+        }
+        if(jumpCount>20){
             clearInterval(jumpInterval);
             jumping=0;
-            counter=0;
+            jumpCount=0;
         }
-        if((characterTop>6)&&(counter<15)){
-            character.style.top = (characterTop-5) + "px";
-        }
-        counter++;
+        jumpCount++;
     },10);
 }
